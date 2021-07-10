7b6a4c859d48fbe90098d4fcb5f2d96355ad48d0
anshultyag <anshultyagi1998@gmail.com>
2021-07-10 00:43:13 -1100

    day9

---
+++ uc7_refactor_the_Code_to_write_a_function_to_get_working_hours.sh
@@ -0,0 +1,13 @@
+workinghr=0
+wage=20
+function getWorkingHr()
+{
+  r=$((RANDOM%2))
+  for(( day = 1; day <= 20; day++ ))
+   do
+        workinghr=4
+  total=$(( total + ( wage * workinghr)))
+   done
+  echo $total
+}
+getWorkingHr
